1d9994a480d562f7cefcb95ed4bc11525d9abdad
Madesh <madeshdex10@gmail.com>
2025-02-09 17:33:00 +0530

    Routing

--- Routing-in-Express/server.js
+++ Routing-in-Express/server.js
@@ -40,55 +40,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 //server static file
-app.use(express.static(path.join(__dirname,'/public')))
+// '/' default which is apply to public folder not subdir
+app.use('/',express.static(path.join(__dirname,'/public')))
+app.use('/subdir',express.static(path.join(__dirname,'/public')))
 
-// optional(.html) ?
-app.get('^/$|/index(.html)?',(req,res)=>{
-    // res.sendFile('./views/index.html',{root:__dirname})
-
-    res.sendFile(path.join(__dirname,'views','index.html'))
-})
-
-// Node js server redirect
-app.get('/new-page(.html)?',(req,res)=>{
-
-    res.sendFile(path.join(__dirname,'views','new-page.html'))
-})
-
-//express js server redirect
-// 301 e resource has been permanently moved to a new location
-// 302 A 302 (found) status means the resource is temporarily located elsewhere.6 
-app.get('/old-page(.html)?',(req,res)=>{
-
-    res.redirect(301,'/new-page.html') // default 302
-})
-
-//Route Handler
-
-app.get('/hello(.html)?',(req,res,next)=>{
-    console.log('attempt to load the hello.html')
-    next()
-},(req,res)=>{
-    res.send('Hello World')
-})
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three])
+//routes for provied to the require(Router which we provied)
+//routes
+app.use('/subdir',require('./routes/subdir'))
+app.use('/',require('./routes/root')) 
+app.use('/employees',require('./routes/api/employees'))
 
 //request all route with using all()
 app.all('*',(req,res)=>{
